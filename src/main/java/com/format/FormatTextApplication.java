ace74989151d38c0a7de6ed34e82ed3e39802343
Ankit <asraniinfo@gmail.com>
2021-02-19 19:34:30 +0100

    empty repo

---
+++ src/main/java/com/format/FormatTextApplication.java
@@ -0,0 +1,13 @@
+package com.format.format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/format/FormatTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.format.format;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
