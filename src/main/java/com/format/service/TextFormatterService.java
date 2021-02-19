b921324236d372e43d72cf0393b4048f8cd96a2b
Ankit <asraniinfo@gmail.com>
2021-02-19 19:49:09 +0100

    code clean up

--- src/main/java/com/format/service/TextFormatterService.java
+++ src/main/java/com/format/service/TextFormatterService.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -23,22 +23,17 @@ public class TextFormatterService {
         } else if (!TextUtil.validAlignmentType(type)) {
             throw new UnknownAlignmentException(String.format("invalid alignment type %s", type));
         }
-
         String normalizedText = StringUtils.normalizeSpace(text);
         List<String> splitText = TextUtil.splitTextWithWordBreak(normalizedText, length);
-        List<String> alignedTextList = new ArrayList<>();
-        splitText.stream()
-                .iterator()
-                .forEachRemaining(part -> {
+        return splitText.stream()
+                .map(part -> {
                     if ("LEFT".equalsIgnoreCase(type)) {
-                        alignedTextList.add(StringUtils.rightPad(part, length));
+                        return StringUtils.rightPad(part, length);
                     } else if ("RIGHT".equalsIgnoreCase(type)) {
-                        alignedTextList.add(StringUtils.leftPad(part, length));
-                    } else if ("CENTER".equalsIgnoreCase(type)) {
-                        alignedTextList.add(StringUtils.center(part, length));
+                        return StringUtils.leftPad(part, length);
                     }
-                });
-        return String.join("\n", alignedTextList);
+                    return StringUtils.center(part, length);
+                }).collect(Collectors.joining("\n"));
     }
 
 
